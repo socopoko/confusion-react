7dd82e026de29655ca95dfbb58948a42793035a3
socopoko <59167401+socopoko@users.noreply.github.com>
2020-06-29 18:24:25 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
+
 
 
 
@@ -13,6 +15,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu/>
       </div>
     );
   }
