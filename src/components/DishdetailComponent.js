d7fc2fec1c86ce10af5b4954efd360d2c17962ad
socopoko <59167401+socopoko@users.noreply.github.com>
2020-07-08 23:17:02 +0530

    single page application part two

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
     function RenderDish({dish}){
@@ -17,8 +17,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         )   
     }
 
-    function RenderComments({dish}) {
-        const comments = dish.comments.map((comment) => {
+    function RenderComments({comments}) {
+        const mycomments = comments.map((comment) => {
             return(
                 <ul key={comment.id} className="list-unstyled">
                     <li className="mb-2">{comment.comment}</li>
@@ -32,18 +32,28 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         return (
             <div className="col-12 col-md-5 m-1"> 
                 <h4>Comments</h4>
-                { comments }
+                { mycomments }
             </div>
         )
     }
 
-    const Dishdetail = (props) => {
+    const DishDetail = (props) => {
         if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
                         <RenderDish dish={props.dish} />
-                        <RenderComments dish={props.dish} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             );
@@ -51,5 +61,5 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         else { return (<div> </div>); }        
     }
 
-export default Dishdetail; 
+export default DishDetail; 
 
