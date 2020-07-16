ae23ced1d4c6fb136d4a78af225b112228e779ab
socopoko <59167401+socopoko@users.noreply.github.com>
2020-07-16 14:41:04 +0530

    Assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentFormComponent';
 
 
     function RenderDish({dish}){
@@ -33,6 +34,7 @@ import { Link } from 'react-router-dom';
             <div className="col-12 col-md-5 m-1"> 
                 <h4>Comments</h4>
                 { mycomments }
+                <CommentForm />
             </div>
         )
     }
